48d4d7d59496ce6832c1245c7e45ddfc4dc89723
quetzle <buizbuizbuizbuiz@gmail.com>
2014-04-20 17:21:54 -0400

    click to select generated names
    click on the names to select all of them in that section

--- game/demos/gens.js
+++ game/demos/gens.js
@@ -39,3 +39,14 @@ function boot() {
 	getTeam();
 	getBoth();
 }
+function selectText(containerid) {
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+     }
+}
